a73094069a5ca4606a97bf5ce43a2832be0b2d98
Mike <mtastle@hotmail.com>
2022-07-19 21:16:29 +0100

    Added logs to user controller

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -24,34 +24,61 @@ namespace AuthService.Controllers
         [HttpPost("/generateToken")]
         public async Task<ActionResult<TokenResponseDto>> GenerateToken(TokenCreateDto loginDetails)
         {
-            TokenResponseDto token = await tokenService.GenerateToken(loginDetails);
+            try
+            {
+                TokenResponseDto token = await tokenService.GenerateToken(loginDetails);
 
-            if (token == null)
-                return NotFound();
+                if (token == null)
+                    return NotFound();
 
-            return Ok(token);
+                return Ok(token);
+            } catch (Exception ex)
+            {
+                logger.LogError(ex, "Error during {method}", "GenerateToken");
+
+                return BadRequest("There was an error");
+            }
+            
         }
 
         [HttpGet("/token/{tokenId}")]
         public async Task<ActionResult<UserResponseDto>> GetUserFromToken(string tokenId)
         {
-            UserResponseDto user = await tokenService.GetUserFromTokenId(tokenId);
+            try
+            {
+                UserResponseDto user = await tokenService.GetUserFromTokenId(tokenId);
 
-            if (user == null)
-                return NotFound();
+                if (user == null)
+                    return NotFound();
 
-            return Ok(user);
+                return Ok(user);
+            } catch (Exception ex)
+            {
+                logger.LogError(ex, "Error during {method}", "GetUserFromToken");
+
+                return BadRequest("There was an error");
+            }
+            
         }
 
         [HttpPost("/refreshToken/{tokenId}")]
         public async Task<ActionResult<TokenResponseDto>> RefreshToken(string tokenId)
         {
-            TokenResponseDto token = await tokenService.RefreshToken(tokenId);
+            try
+            {
+                TokenResponseDto token = await tokenService.RefreshToken(tokenId);
 
-            if (token == null)
-                return NotFound();
+                if (token == null)
+                    return NotFound();
 
-            return Ok(token);
+                return Ok(token);
+            } catch (Exception ex)
+            {
+                logger.LogError(ex, "Error during {method}", "RefreshToken");
+
+                return BadRequest("There was an error");
+            }
+            
         }
 
         [HttpPost]
@@ -59,21 +86,26 @@ namespace AuthService.Controllers
         {
             try
             {
-                logger.LogInformation("Create User Called for email: {email}", userToCreate.Email);
                 UserResponseDto user = await usersService.Create(userToCreate);
 
                 return Ok(user);
             } 
             catch(ArgumentException ex)
             {
+                logger.LogError(ex, "Error during {method}", "Create");
+
                 return BadRequest(ex.Message);
             }
             catch(AlreadyExistsException ex)
             {
+                logger.LogError(ex, "Error during {method}", "Create");
+
                 return BadRequest("The email provided is already in use");
             }
             catch(Exception ex)
             {
+                logger.LogError(ex, "Error during {method}", "Create");
+
                 return BadRequest("Something went wrong");
             }
             
